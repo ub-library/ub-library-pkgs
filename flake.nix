8a007d3d9fcea5673f87586010dac06ed82f925f
Daniel Sandbecker <daniel.sandbecker@hb.se>
2026-01-20 10:12:45 +0100

    Update nixpkgs to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Software for libraries packaged for Nix by University of Borås";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
