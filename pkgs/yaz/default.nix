e32bda61690bbc0417409a6aeeb8c94862a05d1a
Daniel Sandbecker <daniel.sandbecker@hb.se>
2026-01-20 10:16:02 +0100

    Update yaz to v5.37.0

--- pkgs/yaz/default.nix
+++ pkgs/yaz/default.nix
@@ -11,11 +11,11 @@
 
 stdenv.mkDerivation rec {
   pname = "yaz";
-  version = "5.35.1";
+  version = "5.37.0";
 
   src = fetchurl {
     url = "http://ftp.indexdata.com/pub/yaz/${pname}-${version}.tar.gz";
-    hash = "sha256-2wMNbWaIA5ikQhXiYTJjDulPXkYtg4gJ5D+X5jmcE1M=";
+    hash = "sha256-klf+sG4v27/Ot9BAwTn6E5V8TR67pqopOm3RPKsiJc4=";
   };
 
   buildInputs = [
